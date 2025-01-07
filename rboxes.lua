9b8516b946aded2566417e582a182b0dcf7368c4
Jaff <UntitledJaffies@gmail.com>
2025-01-08 01:44:48 +0500

    Fixed, now works

--- rboxes.lua
+++ rboxes.lua
@@ -1,25 +1,30 @@
 local rBoxes = {}
 
+local smooth = 2 -- Smoothes out rounded boxes. The higher the number - the higher the smoothing rate.
+
 do
 	local mat = Material('rboxes/simple')
 	local mat2 = Material('rboxes/mask')
 
-	local setInt = mat.SetInt
+	local setFloat = mat.SetFloat
 
-	setInt(mat, '$c0_x', ScrW())
-	setInt(mat, '$c0_y', ScrH())
+	setFloat(mat, '$c0_x', ScrW() / smooth)
+	setFloat(mat, '$c0_y', ScrH() / smooth)
+
+	setFloat(mat2, '$c0_x', ScrW() / smooth)
+	setFloat(mat2, '$c0_y', ScrH() / smooth)
 
 	hook.Add('OnScreenSizeChanged', 'rboxes.screenSizeChanged' .. SysTime(), function(_, _, newW, newH)
-		setInt(mat, '$c0_x', newW)
-		setInt(mat, '$c0_y', newH)
+		setFloat(mat, '$c0_x', newW / smooth)
+		setFloat(mat, '$c0_y', newH / smooth)
 
-		setInt(mat2, '$c0_x', newW)
-		setInt(mat2, '$c0_y', newH)
+		setFloat(mat2, '$c0_x', newW / smooth)
+		setFloat(mat2, '$c0_y', newH / smooth)
 	end)
 
 	local setDrawColor = surface.SetDrawColor
 	local setMaterial = surface.SetMaterial
-	local drawTexturedRect = surface.DrawTexturedRect
+	local drawTexturedRectUV = surface.DrawTexturedRectUV
 
 	---@param radius number
   ---@param x integer
@@ -28,13 +33,13 @@ do
   ---@param h integer
   ---@param color Color
 	function rBoxes.drawRoundedBox(radius, x, y, w, h, color)
-		setInt(mat, '$c0_z', radius / w)
-		setInt(mat, '$c0_w', radius / h)
+		setFloat(mat, '$c0_z', radius / w * 2)
+		setFloat(mat, '$c0_w', radius / h * 2)
 
 		setDrawColor(color)
 		setMaterial(mat)
 
-		drawTexturedRect(x, y, w, h)
+		drawTexturedRectUV(x, y, w, h, -0.015625, -0.015625, 1.015625, 1.015625)
 	end
 
 	local setTexture = mat.SetTexture
@@ -47,14 +52,14 @@ do
   ---@param color Color
   ---@param texture ITexture
 	function rBoxes.drawRoundedBoxTextured(radius, x, y, w, h, color, texture)
-		setInt(mat2, '$c0_z', radius / w)
-		setInt(mat2, '$c0_w', radius / h)
+		setFloat(mat2, '$c0_z', radius / w)
+		setFloat(mat2, '$c0_w', radius / h)
 		setTexture(mat2, '$basetexture', texture)
 
 		setDrawColor(color)
 		setMaterial(mat2)
 
-		drawTexturedRect(x, y, w, h)
+		drawTexturedRectUV(x, y, w, h, -0.015625, -0.015625, 1.015625, 1.015625)
 	end
 end
 
